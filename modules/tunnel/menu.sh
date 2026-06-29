3d5017a6029ee250e9fdbf9cb01e02e94a363d9f
onlyfortest2424 <onlyfortest2424@gmail.com>
2026-06-29 23:27:17 +0330

    feat: add tunnel module

---
+++ modules/tunnel/menu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear
+
+echo "======================================"
+echo "         TUNNEL MODULE"
+echo "======================================"
+echo
+echo "Coming Soon..."
+echo
+read -p "Press Enter to return..."
