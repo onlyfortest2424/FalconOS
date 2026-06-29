afbf3713ad879c5e47e0511e0ac7a237762bdf46
onlyfortest2424 <onlyfortest2424@gmail.com>
2026-06-29 23:26:22 +0330

    feat: add panel module

---
+++ modules/panel/menu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear
+
+echo "======================================"
+echo "          PANEL MODULE"
+echo "======================================"
+echo
+echo "Coming Soon..."
+echo
+read -p "Press Enter to return..."
