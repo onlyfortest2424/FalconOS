d7c9de02dcc24f3f14817a4f738f22e1cb21b446
Only <onlyfortest2424@gmail.com>
2026-07-08 01:14:40 +0330

    Update FalconOS

--- tunnel/backhaul/install.sh
+++ tunnel/backhaul/install.sh
@@ -2,9 +2,59 @@
 
 install_backhaul() {
 
+while true; do
+
     title "Backhaul Installation"
+
+    echo "[1] Install Server"
+    echo "[2] Install Client"
+    echo
+    echo "[0] Back"
+    echo
+
+    read -rp "Choose: " opt
+
+    case "$opt" in
+
+        1)
+            install_backhaul_server
+        ;;
+
+        2)
+            install_backhaul_client
+        ;;
+
+        0)
+            return
+        ;;
+
+        *)
+            warn "Invalid Option"
+            sleep 1
+        ;;
+
+    esac
+
+done
+
+}
+
+install_backhaul_server() {
+
+    title "Backhaul Server Installation"
 
     warn "Coming Soon"
 
     pause
+
+}
+
+install_backhaul_client() {
+
+    title "Backhaul Client Installation"
+
+    warn "Coming Soon"
+
+    pause
+
 }
