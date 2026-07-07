75e08a4f7767f9cb9c420785ba4a7c43629c0025
Only <onlyfortest2424@gmail.com>
2026-07-08 01:30:40 +0330

    Update FalconOS

--- tunnel/backhaul/install.sh
+++ tunnel/backhaul/install.sh
@@ -6,8 +6,8 @@ while true; do
 
     title "Backhaul Installation"
 
-    echo "[1] Install Server (Kharej / جراخ)"
-    echo "[2] Install Client (Iran / ناریا)"
+    echo "[1] Install Server (Kharej)"
+    echo "[2] Install Client (Iran)"
     echo
     echo "[0] Back"
     echo
@@ -39,15 +39,66 @@ done
 
 }
 
+#########################################
+# Install Backhaul Server
+#########################################
+
 install_backhaul_server() {
 
     title "Backhaul Server Installation"
 
-    warn "Coming Soon"
+    echo "Checking system..."
+    echo
+
+    # Root Check
+    if [[ $EUID -ne 0 ]]; then
+        error "Please run FalconOS as root."
+        return
+    fi
+    ok "Root Access"
+
+    # Detect Architecture
+    ARCH=$(uname -m)
+
+    case "$ARCH" in
+        x86_64)
+            ARCH="amd64"
+        ;;
+        aarch64)
+            ARCH="arm64"
+        ;;
+        armv7l)
+            ARCH="armv7"
+        ;;
+        *)
+            error "Unsupported architecture: $ARCH"
+            return
+        ;;
+    esac
+
+    ok "Architecture : $ARCH"
+
+    # Detect OS
+    if [[ -f /etc/os-release ]]; then
+        . /etc/os-release
+        OS="$NAME"
+    else
+        OS="Unknown"
+    fi
+
+    ok "Operating System : $OS"
+
+    echo
+    echo "System is ready for Backhaul installation."
+    echo
 
     pause
 
 }
+
+#########################################
+# Install Backhaul Client
+#########################################
 
 install_backhaul_client() {
 
