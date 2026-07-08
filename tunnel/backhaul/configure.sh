9dcac1b4e199bd204dfee1ceb8725c9c692e5ac6
Only <onlyfortest2424@gmail.com>
2026-07-09 00:44:27 +0330

    Update FalconOS

--- tunnel/backhaul/configure.sh
+++ tunnel/backhaul/configure.sh
@@ -45,7 +45,104 @@ configure_backhaul_server() {
 
     title "Backhaul Server Configuration"
 
-    warn "Coming Soon"
+    echo "Transport"
+    echo
+    echo "[1] TCP"
+    echo "[2] WebSocket"
+    echo "[3] TCP + TLS"
+    echo "[4] gRPC"
+    echo
+    echo "[0] Back"
+    echo
+
+    read -rp "Choose: " transport
+
+    case "$transport" in
+
+        1)
+            TRANSPORT="tcp"
+        ;;
+
+        2)
+            TRANSPORT="ws"
+        ;;
+
+        3)
+            TRANSPORT="tcp"
+        ;;
+
+        4)
+            TRANSPORT="grpc"
+        ;;
+
+        0)
+            return
+        ;;
+
+        *)
+            warn "Invalid Option"
+            pause
+            return
+        ;;
+
+    esac
+
+    ask_server_port
+
+}
+
+#########################################
+
+ask_server_port() {
+
+    title "Backhaul Server Configuration"
+
+    echo "Transport : $TRANSPORT"
+    echo
+
+    read -rp "Listen Port : " SERVER_PORT
+
+    ask_server_token
+
+}
+
+#########################################
+
+ask_server_token() {
+
+    title "Backhaul Server Configuration"
+
+    echo "Transport : $TRANSPORT"
+    echo "Port      : $SERVER_PORT"
+    echo
+
+    read -rp "Token : " TOKEN
+
+    ask_server_mapping
+
+}
+
+#########################################
+
+ask_server_mapping() {
+
+    title "Backhaul Server Configuration"
+
+    echo "Example"
+    echo
+    echo "443=127.0.0.1:443"
+    echo
+
+    read -rp "Port Mapping : " PORTS
+
+    echo
+    ok "Wizard completed."
+
+    echo
+    echo "Transport : $TRANSPORT"
+    echo "Port      : $SERVER_PORT"
+    echo "Token     : $TOKEN"
+    echo "Mapping   : $PORTS"
 
     pause
 
